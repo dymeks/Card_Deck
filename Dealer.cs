88a66202b8c5b8a20cea3872ccbbde8ee6fd2d89
Susan_Dymek <scdymek@msn.com>
2018-04-05 10:56:02 -0700

    created a dealer class

---
+++ Dealer.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+namespace Card_Deck 
+{
+    public class Dealer : Player
+    {
+        public Dealer(string name = "dealer") : base(name)
+        {
+
+        }
+
+        public void hitStay()
+        {
+            foreach(Card card in hand)
+            {
+                
+            }
+        }
+    }
+}
